9641ce6d82e03843bb59e715da4df4a8772fbcec
enzoburgos <enzoburgos960@gmail.com>
2023-06-27 00:11:05 -0300

    commit 2 de prueba

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  //todo js
+
+  let saludo= 'hola bienvenidos a mi web'
+  
+  function hacerclick(params) {
+    console.log('usted hizo click')  
+  }
+
+  return (
+    //jsx
+    <div>{saludo}
+
+    <button onClick={hacerclick}>hace click</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import "./Header.css"
+function Header(params) {
+    return(
+        <header>
+            <h2>Logo</h2>
+            <nav>
+                <a href="">Home</a>
+                <a href="">Home</a>
+                <a href="">Home</a>
+                
+            </nav>
+        </header>
+    )    
+}
+
+export default Header
